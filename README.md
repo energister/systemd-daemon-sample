07136f312c4ac3158dc3806335f2cc82a891267c
energister <energister@users.noreply.github.com>
2020-11-18 14:21:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# systemd ready daemon sample written on Java
+This simple daemon demonstrates how to handle stop signals from the systemd.
+
+More info can be found in [this](https://mincong.io/2018/07/03/create-systemd-unit-file-for-java/) article.
---
+++ app/src/main/java/energister/systemd/daemon/sample/GracefulShutdownService.java
@@ -0,0 +1,42 @@
+package energister.systemd.daemon.sample;
+
+import java.io.IOException;
+import java.util.concurrent.Semaphore;
+
+public class GracefulShutdownService {
+    public static void main(String[] args) throws InterruptedException {
+
+        Runtime.getRuntime().addShutdownHook(
+                /**
+                 * Shutdown hooks are executed:
+                 *     When last app's non-daemon thread terminates
+                 *     When System.exit() has been called from Java code
+                 *     Because user hit CTRL-C
+                 *     System level shutdown or User Log-Off
+                 */
+                new Thread(() -> {
+                    System.out.println("Shutdown Hook has been executed");
+
+                    // put shutdown code here
+
+                    purgeInputBuffer();
+                })
+        );
+
+        System.out.println("Service started");
+        System.out.println("Press Ctrl-C to exit");
+        new Semaphore(0).acquire();
+        // don't put anything here: only shutdown hooks will execute until the end
+    }
+
+    private static void purgeInputBuffer() {
+        try {
+            int length = System.in.available();
+            for (int i = 0; i < length; i++) {
+                System.in.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
